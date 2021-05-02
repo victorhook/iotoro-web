49795aabea60ab62b43e625ffb3680af8da3de2a
Victor <victorkrook96@gmail.com>
2021-05-02 18:00:36 +0200

    Added tooltip and help text for inputs.

--- iotoro_web/device/forms.py
+++ iotoro_web/device/forms.py
@@ -7,3 +7,14 @@ class DeviceForm(ModelForm):
     class Meta:
         model = models.Device
         fields = ['name', 'type', 'device_key']
+        labels = {
+            'name': 'Device name',
+            'type': 'Device type',
+            'device_key': 'Device key',
+        }
+        help_texts = {
+            'name': 'A unique name for the device. This could be anything!',
+            'type': 'What type of device it is, eg ESP8266, ESP32 etc.',
+            'device_key': 'A 16-byte long key that is used for ' + 
+            'authenticating the device. Valid values are hex, so 0-f'
+        }
