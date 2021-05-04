ff7b70a9314f1ca8d42effb64328afbeccd75162
Victor <victorkrook96@gmail.com>
2021-05-04 12:14:27 +0200

    Admin register new message models

--- iotoro_web/restapi/admin.py
+++ iotoro_web/restapi/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from protocol import models
 
-admin.site.register(models.Message)
+admin.site.register(models.MessageDownStream)
+admin.site.register(models.MessageUpStream)
